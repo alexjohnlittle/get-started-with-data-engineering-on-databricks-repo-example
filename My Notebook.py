aae9c6697c31f9d7f7313709732d989582d8d98c
john.little@unilever.com <john.little@unilever.com>
2024-08-01 15:03:57 +0000

    Latest Commit

--- My Notebook.py
+++ My Notebook.py
@@ -2,3 +2,22 @@
 # MAGIC %md
 # MAGIC ### This is my first notebook
 # MAGIC
+
+# COMMAND ----------
+
+print("This is a notebook")
+
+# COMMAND ----------
+
+# MAGIC %run ./Includes/Classroom-Setup-05
+
+# COMMAND ----------
+
+display((
+  spark.read.csv(f"{DA.paths.datasets}/customers.csv")
+  ))
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT * FROM csv.`dbfs:/mnt/dbacademy-datasets/get-started-with-data-engineering-on-databricks/v01/customers.csv`
